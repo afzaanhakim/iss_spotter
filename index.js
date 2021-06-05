aa200d7deb2f1a2df9ebaad2d76f832a21602c00
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-05 14:04:57 +0000

    linting index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // index.js
 const { nextISSTimesForMyLocation } = require("./iss");
 
-const printPassTimes = function (passTimes) {
+const printPassTimes = function(passTimes) {
   for (let pass of passTimes) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
     const duration = pass.duration;
-    console.log(`Predicted Future Passes:  ${datetime} for ${duration} seconds!`);
+    console.log(
+      `Predicted Future Passes:  ${datetime} for ${duration} seconds!`
+    );
   }
 };
 
