64ca6877b17c0122e2e1eefb16713150cd834a93
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-05 15:23:49 +0000

    implemneted require logic for print pass times function and used .catch to catch any errors due to unhandled promise rejections

---
+++ index2.js
@@ -0,0 +1,9 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyoverTimes, nextISSTimesForMyLocation } = require("./iss_promised");
+const {printPassTimes} = require('./index')
+
+nextISSTimesForMyLocation().then((passTimes) => {
+  printPassTimes(passTimes);
+})
+.catch((error) => {
+  console.log("It didn't work: ", error.message);
+});
