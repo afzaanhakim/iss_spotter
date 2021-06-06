0f55afe074441646e174a8fa29d0b8423d77f10f
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-06 23:57:58 +0000

    removed additional function call to print passtimes

--- index.js
+++ index.js
@@ -12,11 +12,5 @@ const printPassTimes = function(passTimes) {
   }
 };
 
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  printPassTimes(passTimes);
-});
 
 module.exports = {printPassTimes}
